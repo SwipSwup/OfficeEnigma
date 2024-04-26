d28f1402d8927a19f32804b307c1e2cff07e0365
David Veigel <if23b030@technikum-wien.at>
2024-04-26 14:14:32 +0200

    guad

--- Assets/Scripts/Core/FloorManager/FloorManager.cs
+++ Assets/Scripts/Core/FloorManager/FloorManager.cs
@@ -5,49 +5,74 @@ namespace Core.FloorManager
     public class FloorManager : MonoBehaviour
     {
         [SerializeField] private GameObject baseFloorPrimitives;
+        [SerializeField] private GameObject endFloorPrimitives;
         [SerializeField] private GameObject baseFloorProps;
+        [SerializeField] private GameObject endFloorProps;
 
         [SerializeField] private int startFloorLevel = 10;
 
         private int _currentFloorLevel;
 
-        private GameObject _loadedFloor;
+        private GameObject _loadedFloorProps;
+        private GameObject _loadedFloorPrimitives;
         public int StartFloorLevel => startFloorLevel;
         public int CurrenFloorLevel => _currentFloorLevel;
 
         private void Start()
         {
-            Instantiate(baseFloorPrimitives);
-            _loadedFloor = Instantiate(baseFloorProps);
+            SpawnFloorPrimitives(baseFloorPrimitives);
+            SpawnFloorProps(baseFloorProps);
             _currentFloorLevel = startFloorLevel;
         }
 
-        public void SpawnNextFloor(bool resetIfAnomaly)
+        public void SpawnNextFloor(string resetTag)
         {
-            Debug.Log(_currentFloorLevel);
-
-            if (resetIfAnomaly && _loadedFloor.CompareTag("Anomaly"))
+            if (CurrenFloorLevel <= 0)
+            {
+                SpawnEndFloor();
+                return;
+            } 
+            
+            if (_loadedFloorProps.CompareTag(resetTag))
             {
                 _currentFloorLevel = startFloorLevel;
                 Debug.Log("Reset");
             }          
             
+            Debug.Log("Floor: " + _currentFloorLevel);
+
+            
+            
             SpawnFloorAtLevel(_currentFloorLevel--);
+        }
+
+        private void SpawnEndFloor()
+        {
+            SpawnFloorPrimitives(endFloorPrimitives);
+            SpawnFloorProps(endFloorProps);
         }
 
         public void SpawnFloorAtLevel(int floorLevel)
         {
-            SpawnFloor(GameManager.GameManager.AnomalyManager.TryGetAnomaly(floorLevel, out GameObject anomaly)
+            SpawnFloorProps(GameManager.GameManager.AnomalyManager.TryGetAnomaly(floorLevel, out GameObject anomaly)
                 ? anomaly
                 : baseFloorProps);
         }
 
-        private void SpawnFloor(GameObject floor)
+        private void SpawnFloorProps(GameObject floorProps)
         {
-            if(_loadedFloor != null)
-                Destroy(_loadedFloor);
+            if(_loadedFloorProps != null)
+                Destroy(_loadedFloorProps);
             
-            _loadedFloor = Instantiate(floor);
+            _loadedFloorProps = Instantiate(floorProps);
+        }
+        
+        private void SpawnFloorPrimitives(GameObject floorPrimitives)
+        {
+            if(_loadedFloorPrimitives != null)
+                Destroy(_loadedFloorPrimitives);
+            
+            _loadedFloorPrimitives = Instantiate(floorPrimitives);
         }
     }
 }
